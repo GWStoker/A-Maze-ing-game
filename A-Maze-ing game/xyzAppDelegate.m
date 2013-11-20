81909f3e1c306da7ae394e856f69744205b04e31
GWStoker <mac@Mac-5.local>
2013-11-20 09:08:50 -0700

    operational with some bugs

--- A-Maze-ing game/xyzAppDelegate.m
+++ A-Maze-ing game/xyzAppDelegate.m
@@ -8,7 +8,7 @@
 
 #import "xyzAppDelegate.h"
 
-#import "xyzViewController.h"
+#import "bob.h"
 
 @implementation xyzAppDelegate
 
--- A-Maze-ing game/xyzViewController.m
+++ A-Maze-ing game/xyzViewController.m
@@ -5,18 +5,24 @@
 //  Created by mac on 10/19/13.
 //  Copyright (c) 2013 mac. All rights reserved.
 //
-
-#import "xyzViewController.h"
+#include <stdlib.h>
+#import "bob.h"
 
 @interface xyzViewController ()
 
 @end
 
 @implementation xyzViewController
+{
+    bool m_frozen;
+    CGPoint pacManStart;
+};
 
 - (void)viewDidLoad
 // Movement of pacman
 {
+    pacManStart = CGPointMake (0,144);
+    m_frozen=false;
     self.lastUpdateTime = [[NSDate alloc] init];
     
     self.currentPoint  = CGPointMake(0, 144);
@@ -33,7 +39,6 @@
 
 CGPoint origin1 = self.ghost1.center;
 CGPoint target1 = CGPointMake(self.ghost1.center.x, self.ghost1.center.y-124);
-
 CABasicAnimation *bounce1 = [CABasicAnimation animationWithKeyPath:@"position.y"];
 bounce1.fromValue = [NSNumber numberWithInt:origin1.y];
 bounce1.toValue = [NSNumber numberWithInt:target1.y];
@@ -61,11 +66,12 @@ bounce3.toValue = [NSNumber numberWithInt:target3.y];
 bounce3.duration = 2;
 bounce3.repeatCount = HUGE_VALF;
 bounce3.autoreverses = YES;
-[self.ghost3.layer addAnimation:bounce3 forKey:@"position"];
- }
-{
-[super viewDidLoad]
-	// Do any additional setup after loading the view, typically from a nib.
+    [self.ghost3.layer addAnimation:bounce3 forKey:@"position"];
+    
+ 
+
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
 }
 - (void)didReceiveMemoryWarning
 {
@@ -76,6 +82,9 @@ bounce3.autoreverses = YES;
     
     NSTimeInterval secondsSinceLastDraw = -([self.lastUpdateTime timeIntervalSinceNow]);
     
+    if(m_frozen==false)
+    
+    {
     self.pacmanYVelocity = self.pacmanYVelocity - (self.acceleration.x * secondsSinceLastDraw);
     self.pacmanXVelocity = self.pacmanXVelocity - (self.acceleration.y * secondsSinceLastDraw);
     
@@ -84,8 +93,15 @@ bounce3.autoreverses = YES;
     
     self.currentPoint = CGPointMake(self.currentPoint.x + xDelta,
                                     self.currentPoint.y + yDelta);
-    
-    [self movePacman];
+    }
+    else
+    {
+        self.currentPoint = self.previousPoint;
+        self.pacmanXVelocity=0;
+        self.pacmanYVelocity=0;
+        
+    }
+        [self movePacman];
     
     self.lastUpdateTime = [NSDate date];
     
@@ -149,18 +165,43 @@ bounce3.autoreverses = YES;
     
     if (CGRectIntersectsRect(self.pacman.frame, self.exit.frame)) {
         
-        [self.motionManager stopAccelerometerUpdates];
         
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Congratulations"
-                                                        message:@"You've won the game!"
+        
+        self.currentPoint  = pacManStart;
+        m_frozen=true;
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Congratulations!"
+                                                        message:@"Start new level?"
                                                        delegate:self
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil];
+                                              cancelButtonTitle:@"Yes"
+                                              otherButtonTitles:@"No", nil];
         [alert show];
+        
+               
+        [self changeLevel];
         
     }
     
 }
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{if (buttonIndex == [alertView cancelButtonIndex])
+{
+    m_frozen=false;
+  }
+else
+{ [self changeLevel];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Do you like this level?"
+                                                    message:@"Start new level?"
+                                                   delegate:self
+                                          cancelButtonTitle:@"Yes"
+                                          otherButtonTitles:@"No", nil];
+    [alert show];
+ 
+    
+}
+}
+
+
 - (void)collisionWithGhosts {
     
     CALayer *ghostLayer1 = [self.ghost1.layer presentationLayer];
@@ -171,18 +212,27 @@ bounce3.autoreverses = YES;
         || CGRectIntersectsRect(self.pacman.frame, ghostLayer2.frame)
         || CGRectIntersectsRect(self.pacman.frame, ghostLayer3.frame) ) {
         
-        self.currentPoint  = CGPointMake(0, 144);
         
+
+         self.currentPoint  = pacManStart;
+        
+        m_frozen=true;
+
         UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Oops!"
                                                         message:@"Mission Failed!"
                                                        delegate:self
-                                              cancelButtonTitle:@"OK"
-                                              otherButtonTitles:nil];
+                                              cancelButtonTitle:@"Continue"
+                                              otherButtonTitles:@"Give up",nil];
         [alert show];
-        
+       
     }
     
+     
 }
+
+
+    
+
 - (void)collsionWithWalls {
     
     CGRect frame = self.pacman.frame;
@@ -211,8 +261,83 @@ bounce3.autoreverses = YES;
             
         }
         
+    
+}
+}
+-(void)changeLevel
+{
+      pacManStart = CGPointMake(arc4random()%480,arc4random()%360);
+    self.currentPoint =pacManStart;
+    
+	CGPoint exitPoint = CGPointMake(arc4random() %480,arc4random() %360);
+    self.exit.center=exitPoint;
+
+    for (UIImageView *image in self.wall)
+    {
+        bool xBad;
+        xBad=true;
+        int x=0;
+        int y=0;
+        while(xBad==true)
+        {x = arc4random() % 480;
+        y = arc4random() % 320;
+            if (((abs(x-pacManStart.x)<40) && (abs(y-pacManStart.y)<40))||
+                ((abs(x-self.exit.center.x)<40)&&(abs(y-self.exit.center.y)<40 ))){
+                xBad=true;
+                
+            }
+            else
+            {xBad=false;
+                
+            }
+        }
+        image.center=CGPointMake(x,y);
+    
     }
+
+    CGPoint origin1 = CGPointMake(arc4random()%480, arc4random()%360);
+    CGPoint target1 = CGPointMake(arc4random()%480, arc4random()%360);
+    CABasicAnimation *bounce1 = [CABasicAnimation animationWithKeyPath:@"position"];
+    bounce1.fromValue = [NSValue valueWithCGPoint:origin1];
+    bounce1.toValue = [NSValue valueWithCGPoint:target1];
+    bounce1.duration = arc4random()%10;
+    bounce1.autoreverses = YES;
+    bounce1.repeatCount = HUGE_VALF;
+    
+    [self.ghost1.layer addAnimation:bounce1 forKey:@"position"];
+    
+    CGPoint origin2 = CGPointMake(arc4random()%480, arc4random()%360);
+    CGPoint target2 = CGPointMake(arc4random()%480, arc4random()%360);
+    CABasicAnimation *bounce2 = [CABasicAnimation animationWithKeyPath:@"position"];
+    bounce2.fromValue = [NSValue valueWithCGPoint:origin2];
+    bounce2.toValue = [NSValue valueWithCGPoint:target2];
+    bounce2.duration = arc4random()%10;
+    bounce2.repeatCount = HUGE_VALF;
+    bounce2.autoreverses = YES;
+    [self.ghost2.layer addAnimation:bounce2 forKey:@"position"];
+    
+    CGPoint origin3 = CGPointMake(arc4random()%480, arc4random()%360);
+    CGPoint target3 = CGPointMake(arc4random()%480, arc4random()%360);
+    CABasicAnimation *bounce3 = [CABasicAnimation animationWithKeyPath:@"position"];
+    bounce3.fromValue = [NSValue valueWithCGPoint:origin3];
+    bounce3.toValue = [NSValue valueWithCGPoint:origin3];
+    bounce3.duration = arc4random()%10;
+    bounce3.repeatCount = HUGE_VALF;
+    bounce3.autoreverses = YES;
+    [self.ghost3.layer addAnimation:bounce3 forKey:@"position"];
+    
+ 
+    
+    
+//https://developer.apple.com/library/ios/documentation/cocoa/conceptual/coreanimation_guide/CreatingBasicAnimations/CreatingBasicAnimations.html
+
+    
+    
+    
+    
     
 }
 
+
 @end
+
